00d472557a92fe6d2a7966c6fe6822b67b914ae8
Gabriel Vaught <gabeavaught@gmail.com>
2024-03-24 14:32:08 -0500

    Update resetuser remove option to not set users as new

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -90,33 +90,16 @@ export const resetAllUsersCommand: Command = {
 	name: 'resetusers',
 	builder: new SlashCommandBuilder()
 		.setName('resetusers')
-		.setDescription('Remove all books and choose to set all users as new users')
-		.addStringOption(option =>
-			option.setName('isnewusers').setDescription('Mark all users as new?').setRequired(true).addChoices(
-				{
-					name: 'Yes',
-					value: 'yes'
-				},
-				{
-					name: 'No',
-					value: 'no'
-				}
-			)
-		),
+		.setDescription('WARNING: Remove all books and set all users as new users'),
 	handler: async (interaction: ChatInputCommandInteraction) => {
-		const setIsFirstLookup = interaction.options.get('isnewusers')?.value === 'yes';
-
 		await prisma.book.deleteMany();
 
-		if (setIsFirstLookup) {
-			await prisma.user.updateMany({
-				data: {
-					isFirstLookup: true
-				}
-			});
-		}
+		await prisma.user.updateMany({
+			data: {
+				isFirstLookup: true
+			}
+		});
 
-		console.log('Reset all users');
 		interaction.reply('Reset all users.');
 	}
 };
