0f60a0a10862075b7c5b09714be60a1f66944306
Gabriel Vaught <gabeavaught@gmail.com>
2024-08-13 14:56:47 -0500

    Change from cron to croner, possible mem leak

--- src/batch/run.ts
+++ src/batch/run.ts
@@ -1,17 +1,17 @@
-import { CronJob } from 'cron';
+import Cron from 'croner';
 import { Client } from 'discord.js';
 import { scrapeBooks } from './scraper';
 
 export function runBatch(client: Client) {
-	const updateBooks = new CronJob(
+	const job = new Cron(
 		'*/5 * * * *', //Every 5 minutes
+		{
+			timezone: 'America/Chicago'
+		},
 		async () => {
 			console.log('Starting batch at', new Date().toLocaleString());
 			await scrapeBooks(client);
 			console.log('Finished batch at', new Date().toLocaleString());
-		},
-		null,
-		true,
-		'America/Chicago'
+		}
 	);
 }
